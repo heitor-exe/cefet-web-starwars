b11e799dddd350ad4a8d73cb9d2499fe575a0a37
Heitor Macedo <heitorvmacedo@gmail.com>
2026-05-11 14:43:22 -0300

    Exercicio 1 e 2

--- starwars.js
+++ starwars.js
@@ -1,7 +1,34 @@
-// Seu javascript aqui :)
-// Use a Star Wars API: https://swapi.info/
-// para fazer uma requisição assíncrona e:
-//  - Pegar a lista de filmes (AJAX) e preencher no HTML
-//  - Quando um filme for clicado, exibir sua introdução
+import { play } from "./music.js";
+import { toRoman } from "./roman.js";
 
-const API_ENDPOINT = 'https://swapi.info/api'
+const API_ENDPOINT = "https://swapi.info/api";
+
+play(
+	{
+		audioUrl: "audio/tema-sw.mp3",
+		coverImageUrl: "imgs/logo.svg",
+		title: "Intro",
+		artist: "John Williams",
+	},
+	document.body,
+);
+
+// ordena filmes por número do episódio
+const ordenaFilmes = (a, b) => a.episode_id - b.episode_id;
+
+// elemento <ul> onde os filmes serão adicionados
+const ul = document.querySelector("#filmes ul");
+
+// busca os filmes e cria os <li>
+fetch(`${API_ENDPOINT}/films`)
+	.then((response) => response.json())
+	.then((data) => {
+		data.sort(ordenaFilmes);
+		data.forEach((film) => {
+			const episodeRoman = toRoman(film.episode_id).padEnd(3, " ");
+			const li = document.createElement("li");
+			li.textContent = `Episode ${episodeRoman} - ${film.title}`;
+			ul.appendChild(li);
+		});
+	})
+	.catch((error) => console.error("Erro:", error));
